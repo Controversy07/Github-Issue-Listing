67e68d7afe8acc3041d603e1327b6cb00677e367
Controversy07 <44866974+Controversy07@users.noreply.github.com>
2019-08-27 00:48:40 +0530

    Update src.js

--- src.js
+++ src.js
@@ -33,12 +33,42 @@ function search_repo() //main function for all 3 server calls
   var last_week=dateObj.getFullYear() + '-' + (dateObj.getMonth()+1) + '-' + (dateObj.getDate())+'T'+dateObj.getHours() + ":" + dateObj.getMinutes() + ":" + dateObj.getSeconds() + "Z";
   var $url_last_week = "https://api.github.com/repos/"+split[3]+"/"+split[4]+"/issues?since="+last_week;
   servercall($url_last_week,lastweek_issuecount);
+  /*
 
-  show_result();// dispaly the calculated results
+  Every pull request is an issue, but not every issue is a pull request.
+  For this reason, “shared” actions for both features, like manipulating assignees, labels and milestones, are provided within the Issues API.
+
+  */
+  if(!$("#pull_req_toggle").is(":checked")) //if the user just wants to have a issue count without pull request count
+  {
+    var $pull_url = "https://api.github.com/repos/"+split[3]+"/"+split[4]+"/pulls?state=open";
+    servercall($pull_url,get_lastpage_url);
+  }
+  show_result();// display the calculated results
   return;
 }
 
+function get_lastpage_url(data,xhr)
+{
+  /*  This api uses paging thus the default value is "per_page=30".
+      The maximum is per_page=100. To get more than 100 results, you need to call it multiple times
+      instead we determine total number of pull requests from the last page returned in the Link header.
+      the link headder returns the next url and the last url
+      eg-: ' <https://api.github.com/repositories/{ id } /pulls?state=open&page={ LASTAPGE }>; rel="last" ' where rel indicates relative url
+  */
+  var link=xhr.getResponseHeader("link");
+  link=link.split(','); //separting the last and the next url
+  var $lastpage_url=(link[1].match("<(.*)>")[1])//get the value of the url alone as show in example url
+  servercall($lastpage_url,remove_pull_count);
+}
 
+
+function remove_pull_count(data,xhr,url)
+{
+  var total_page= url.split("page=")[1];//  matching to get the value of the page as show in example url
+  var open_pull_count=((total_page-1)*30)+data.length;
+  $("#issue_total span").text(  $("#issue_total span").html() - open_pull_count);
+}
 
 function blurHandler(zin,opc)   // blur handler
 {
@@ -61,7 +91,7 @@ function servercall(url,callback)// simple js ajax call
         {
           if(callback!=undefined)
           {
-              callback(data);
+              callback(data,xhr,url);
           }
           return;
         },
@@ -85,7 +115,7 @@ function servercall(url,callback)// simple js ajax call
 function show_result()//display the result by hidingb and showing appropiate divs
 {
   blurHandler(0,0);
-  $("#issue_rest span").text($("#issue_total span").text() - $("#issue_lastweek span").text() - $("#issue_today span").text()); //total - lastweek - today
+  $("#issue_rest span").text($("#issue_total span").text() -  $("#issue_lastweek span").text() - $("#issue_today span").text());
   $(".search_form").hide();
   $(".search_section").animate({top: "150px"},function()
   {
@@ -110,7 +140,7 @@ function lastweek_issuecount(resp)
 {
   if(resp!=undefined)
   {
-    $("#issue_lastweek span").text(resp.length -  $("#issue_today span").text());// isuee from last week mius today
+    $("#issue_lastweek span").text(resp.length -  $("#issue_today span").text());
   }
   else
   {
